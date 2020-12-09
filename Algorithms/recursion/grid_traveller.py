f38377e827b461a406c588fcf9caf699fd6ce7c9
Adam Rao <raoadam87@gmail.com>
2020-12-10 01:55:22 +0300

    Fixes bug in grid traveler

--- Algorithms/recursion/grid_traveller.py
+++ Algorithms/recursion/grid_traveller.py
@@ -6,7 +6,7 @@ def grid_traveler(m, n, memo={}):
     if m == 1 and n == 1: return 1
     if m == 0 or n == 0: return 0
 
-    memo[key] = grid_traveler(m-1, n) + grid_traveler(m, n-1)
+    memo[key] = grid_traveler(m-1, n, memo)  + grid_traveler(m, n-1, memo)
 
     return memo[key]
 
